695605255a2caf85a59b178ba560cfde738a0027
anmichi <47889950+anmichi@users.noreply.github.com>
2024-07-09 00:59:37 +0900

    validateした

--- src/direct/train.py
+++ src/direct/train.py
@@ -1,5 +1,11 @@
 import pickle
-import sklearn.linear_model
+from sklearn.linear_model import Ridge
+from sklearn import svm
+from sklearn import metrics
+from sklearn.neural_network import MLPRegressor
+import numpy as np
+
+import pandas as pd
 
 
 class DirectTrainer:
@@ -8,7 +14,7 @@ class DirectTrainer:
 
     def load_data(self):
         train = pickle.load(open("../../data/train_data.pkl", "rb"))
-        test = pickle.load(open("../../data/test_data.pkl", "rb"))
+        test = pickle.load(open("../../data/validation_data.pkl", "rb"))
         X_train = train.iloc[:, 1:301]
         Y_train = train.iloc[:, 309:310]
         X_test = test.iloc[:, 1:301]
@@ -20,8 +26,20 @@ class DirectTrainer:
 
     def generate_model(self):
         X_train, Y_train, X_test, Y_test = self.load_data()
-        self.model = sklearn.linear_model.Ridge().fit(X_train, Y_train)
+        best_score = 0
+        for h in [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]:
+            model = Ridge(alpha=h, random_state=18).fit(X_train, Y_train)
+            score = self.evaluate_model(model, X_test, Y_test)
+            # print(h, score)
+            if best_score < score:
+                best_score = score
+                self.model = model
         self.dump_model()
+
+    def evaluate_model(self, model, X_test, Y_test):
+        Y_predict = pd.DataFrame(data=model.predict(X_test))
+        return metrics.r2_score(Y_test, Y_predict)
 
 
 DirectTrainer().generate_model()
+# r2_score = 0.30488
