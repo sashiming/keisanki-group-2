c517387fa5186de3300b95dd1ed4320a963170b4
sashiming <m.ymd041807@gmail.com>
2024-07-09 15:43:02 +0900

    3つほどモデルを作成

--- src/indirect/emotion.py
+++ src/indirect/emotion.py
@@ -2,26 +2,58 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import Ridge
 from sklearn.multioutput import MultiOutputRegressor
+from sklearn.neural_network import MLPRegressor
+
+# 前半パート...文章データから8種類の感情数値を推定
 
 def _import_train_data():
-    train_file = Path(__file__).parents[2].joinpath('data', 'new_train_data.pkl')
+    train_file = Path(__file__).parents[2].joinpath('data', 'train_data.pkl')
     with train_file.open(mode='rb') as f:
         train = pickle.load(f)
-    print(train.shape)
-    print(train)
     X = train.iloc[:, 1:301]
     y = train.iloc[:, 301:309]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
-    return X_train, X_test, y_train, y_test
+    return X, y
+
+def _dump_model(filename, model):
+    model_path = Path(__file__).parents[2].joinpath('models', filename)
+    with model_path.open(mode='wb') as f:
+        pickle.dump(model, f)
 
 def ridge_model():
-    X_train, X_test, y_train, y_test = _import_train_data()
-    model = MultiOutputRegressor(Ridge(random_state=42))
-    model.fit(X_train, y_train)
-    print(model.predict(X_test)[:20])
+    X_train, y_train = _import_train_data()
+    params = {'estimator__alpha': [0.1, 0.5, 1.0, 5.0, 10.0, 20.0, 50.0]}
+    gscv = GridSearchCV(MultiOutputRegressor(Ridge()),
+                        param_grid=params, cv=5, scoring='neg_mean_squared_error')
+    gscv.fit(X_train, y_train)
+    best_model = gscv.best_estimator_
+    print(' ----- ridge -----')
+    print('best params:', gscv.best_params_)
+    print('best score:', gscv.best_score_)
+    _dump_model('emo_ridge_best.pkl', best_model)
 
-# _import_train_data()
+def xgboost_model():
+    X_train, y_train = _import_train_data()
+    params = {'eta': [0.01, 0.1, 0.2],
+              'gamma': [0, 0.1],
+              'max_depth': [2, 4],}
+    gscv = GridSearchCV(XGBRegressor(),
+                        param_grid=params, cv=3, verbose=3, scoring='neg_mean_squared_error')
+    gscv.fit(X_train, y_train)
+    best_model = gscv.best_estimator_
+    print('best params:', gscv.best_params_)
+    print('best score:', gscv.best_score_)
+    _dump_model('emo_xgboost_best.pkl', best_model)
+
+def neuralnet_model():
+    X_train, y_train = _import_train_data()
+    model = MLPRegressor(hidden_layer_sizes=(100,50,20), max_iter=1000)
+    model.fit(X_train, y_train)
+    _dump_model('emo_neuralnet.pkl', model)
+
 ridge_model()
+xgboost_model()
+neuralnet_model()
